ea2133b6da06de9d001bc874d5b7d76f29165575
Dominique Berry <dominique174466@gmail.com>
2024-04-17 08:41:34 -0400

    Add space as leader functionality and relative line numbers

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
---[[
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -255,6 +254,7 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
+vim.wo.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
